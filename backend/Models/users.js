10635178857f4dc1f556ccbe2c8acb83f068882f
Daniel Yuki <danielyukihiga1@gmail.com>
2024-05-05 13:14:33 -0300

    demo repo setup

---
+++ backend/Models/users.js
@@ -0,0 +1,14 @@
+class Users {
+    constructor( userId, userName, userImg, age, contact, closeContacts, coordinates, data) {
+        (this.userId = userId),
+        (this.userName = userName),
+        (this.userImg = userImg),
+        (this.age = age),
+        (this.contact = contact),
+        (this.closeContacts = closeContacts),
+        (this.coordinates = coordinates),
+        (this.data = data);
+    }
+}
+
+export default Users;
